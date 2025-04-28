f70721c40c1edc5d68ced819a588a7739b1cc6c8
Ayushi Singh <ayushisinghpatel373@gmail.com>
2025-04-29 01:39:17 +0530

    DSA

--- Array_Strivers/2D Array/set0.java
+++ Array_Strivers/2D Array/set0.java
@@ -12,31 +12,34 @@ public class set0 {
             for (int j = 0; j < arr[0].length; j++) {
                 if(arr[i][j] == 0){
                     for(int k = 0;k<arr.length;k++){
-                        temp[i][k] = 0;
+                        //temp[i][k] = 0;
                         temp[k][j] = 0;
+                    }
+                    for(int k = 0;k<arr[0].length;k++){
+                        temp[i][k] = 0;
+                        //temp[k][j] = 0;
                     }
                 }
                 if(temp[i][j] != 0){
                     temp[i][j] = arr[i][j];
                 }
-
             }
         }
-
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = temp[i][j];
+            }
+        }
         for (int i = 0; i < temp.length; i++) {
             for (int j = 0; j < temp[0].length; j++) {
-                System.out.print(temp[i][j]);
+                System.out.print(arr[i][j]);
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        int mat[][] = {
-            {1,1,1},
-            {1,0,1},
-            {1,1,1}
-        };
+        int mat[][] = {{0,1}};
         setmatrixZero(mat);
     }
 }
