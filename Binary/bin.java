894a0bb76e9b10839b2c950ccfbca87b8fb5da54
Ayushi Singh <ayushisinghpatel373@gmail.com>
2025-05-07 16:17:27 +0530

    Create bin.java

---
+++ Binary/bin.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count =1;
+        int n = 
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] == nums[i-1]){
+                count++;
+            }
+            if(count>)
+        }
+    }
+}
